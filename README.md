55c32b7cd707f0d4b7739206e7e144c30a640079
Diego Betto <diego@papion.it>
2025-10-01 14:27:21 +0200

    Step1 - PM2 cluster

---
+++ README.md
@@ -0,0 +1,14 @@
+# Front end
+
+- [ ] creare cartella `frontend`
+- [ ] Pagina HTML della pizzeria
+- [ ] bottone per elenco ordini
+- [ ] bottone per creare un nuovo ordine
+- [ ] ad ogni bottone abbinare funzione che fa il `console.log`
+
+# Back end
+
+- [ ] creare cartella `backend` (o spostare l'eventuale già fatto)
+- [ ] creare server express
+- [ ] creare rotta `/ping`
+- [ ] creare rotte `/prodotti` e `/prodotti/<id>`
---
+++ backend/src/index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import os from 'os';
+
+const port = 3000;
+
+// creo app express
+const app = express();
+
+// creo endpoint ping
+app.get('/ping', (req, res) => {
+  res.send('pong');
+});
+
+app.get('/prodotti', (req, res) => {
+  const prodotti = [
+    { id: 1, nome: 'Prodotto 1', prezzo: 10.0 },
+    { id: 2, nome: 'Prodotto 2', prezzo: 20.0 },
+    { id: 3, nome: 'Prodotto 3', prezzo: 30.0 },
+  ];
+  res.json(prodotti);
+});
+
+app.get('/prodotti/:id', (req, res) => {
+    res.json({ id: 1, nome: 'Prodotto 1', prezzo: 10.0 });
+});
+
+app.get('/heavy', (req, res) => {
+  let count = 0;
+  for (let i = 0; i < 500_000; i++) {
+    count += i;
+  }
+  res.send(`Count: ${count}`);
+});
+
+// avvio server 
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port} - PID: ${process.pid} - CPUs: ${os.cpus().length}`);
+});

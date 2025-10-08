e772f7f2bd99a10378be94285aed3f1ca9131085
Diego Betto <diego@papion.it>
2025-10-08 16:18:05 +0200

    bullmq

---
+++ backend/src/config/redis.js
@@ -0,0 +1,11 @@
+import Redis from 'ioredis'
+
+const redisConfig = {
+    port: 6379,
+    host: 'redis',
+    maxRetriesPerRequest: null,
+}
+
+const redisConnection = new Redis(redisConfig);
+
+export default redisConnection;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,28 @@
 import express from 'express';
 import os from 'os';
+import {Queue, Worker} from 'bullmq';
+import redisConnection from './config/redis.js';
 
 const port = 3000;
-const id = process.env.BACKEND_ID || 'backend-unknown'
+const id = process.env.BACKEND_ID || 'backend-unknown';
+
+// code di BullMQ tramite Redis
+const mainQueue = new Queue('main-queue', {connection: redisConnection});
+
+const mainWorker = new Worker(
+  'main-queue', 
+  async (job) => {
+    console.log(`Start job ${job.id} on backend ${id}`)
+  }, 
+  {connection: redisConnection}
+);
+
+mainWorker.on('completed', (job) => {
+  console.log(`COMPLETED job ${job.id} on backend ${id}`)
+});
+mainWorker.on('failed', (job, err) => {
+  console.log(`FAILED job ${job.id} on backend ${id} with error ${err.message}`)
+});
 
 // creo app express
 const app = express();
@@ -36,6 +56,12 @@ app.get('/heavy', (req, res) => {
     count += i;
   }
   res.send(`Count: ${count}`);
+});
+
+// endpoint per le code
+app.post('/job-for-all', async (req, res) => {
+  const job = await mainQueue.add('job', {name: req.body?.name || 'default'});
+  res.json({jobId: job.id});
 });
 
 // avvio server 
